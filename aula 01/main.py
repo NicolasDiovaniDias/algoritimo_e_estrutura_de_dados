9947c1e94b6434a57522a2a1e2a0e379a1493022
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2025-03-19 10:01:29 -0300

    10:34

--- aula 01/main.py
+++ aula 01/main.py
@@ -1,5 +1,18 @@
 class Carro:
-    def _init_(self, modelo, ano):
+    def __init__(self, modelo, ano = 2006):
         self.modelo = modelo
         self.ano = ano
-        self.kilometragem = 0
+        self.__kilometragem = 0
+    def SetKM(self, km):
+        if km  > self.__kilometragem:
+            self.__kilometragem = km
+    def incrementar(self, km):
+        if km > 0:
+            self.__kilometragem = km
+    def __str__(self):
+        txt = "modelo: "+self.modelo
+        txt +="\nAno: "+str(self.ano)
+        txt +="\nKilometragem: "+str(self.__kilometragem)
+        return txt
+x = Carro("nicolas", 2004)
+print(x)
