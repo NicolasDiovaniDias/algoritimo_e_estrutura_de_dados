27c4800f793365c6ee3b15419faa68837840fa24
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2025-03-26 11:39:32 -0300

    a

--- aula 02/Pedido.py
+++ aula 02/Pedido.py
@@ -8,5 +8,14 @@ class Pedido:
         self.cliente = cli
         self.Produtos = []
 
-pedido1 = Pedido()
-print()
+    def addProd(self, prod):
+        if prod != None and prod.preco>=10:
+            self.Produtos.append(prod)
+        soma = 0.0
+        for p in self.Produtos:
+            soma+= p.preco
+        return soma
+    def __str__(self):
+        txt = "Pedido realizado em: " + str(self.data)
+        # txt += "\nCliente: " + self.cliente.nome
+        txt += "\nCliente: " + str(self.cliente)
