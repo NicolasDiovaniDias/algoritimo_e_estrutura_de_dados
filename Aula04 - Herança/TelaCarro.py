8ee810b04c339d8a8775b1a62fe65d2e13349bff
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2025-04-16 10:56:20 -0300

    aula 4 projeto terminado

--- Aula04 - Herança/TelaCarro.py
+++ Aula04 - Herança/TelaCarro.py
@@ -2,13 +2,17 @@ import sys
 from PyQt5.QtWidgets import *
 from Carro import Carro
 from TelaVeiculo import TelaVeiculo
-from TelaCategoria import TelaCategoria
+#from TelaCategoria import TelaCategoria
+
+
 class TelaCarro( TelaVeiculo ):
 
-    def __init__(self, titulo = "Tela de Carro",categorias = [], telaCat = TelaCategoria()):
+    def __init__(self, titulo = "Tela de Carro", categorias = [], telaCat = None ):
         self.telaCategorias = telaCat
-        self.ListaCategorias = categorias 
+        self.listaCategorias = categorias
         super().__init__(titulo)
+        
+
     def definirLayout(self):
         super().definirLayout()
         self.lblPortas = QLabel( "Qtd Portas: ")
@@ -16,25 +20,24 @@ class TelaCarro( TelaVeiculo ):
         self.layout.addWidget( self.lblPortas )
         self.layout.addWidget( self.txtPortas )
 
-        self.lblCategoria  = QLabel("Categoria:")
-        self.layout.addWidget(self.lblCategoria)
+        self.lblCategoria = QLabel("Categoria:")
+        self.layout.addWidget( self.lblCategoria)
 
         self.cmbCategoria = QComboBox(self)
-        self.cmbCategoria.addItem("Selecione...", None)
-        for cat in self.ListaCategorias:
-            self.cmbCategoria.addItem(cat.nome , cat)
-        self.layout.addWidget(self.cmbCategoria)
+        self.carregarCategorias()
+        self.layout.addWidget(self.cmbCategoria )
 
-        self.btnAddCategoria = QPushButton("Adicionar Cidade",self)
-        self.btnAddCategoria.clicked.connect(self.abrirTelaCategoria)
-        self.layout.addWidget(self.btnAddCategoria)
-        self.layout.addWidget(self.cmbCategoria)
+        self.btnAddCategoria = QPushButton("Adicionar Categoria", self)
+        self.btnAddCategoria.clicked.connect( self.abrirTelaCategoria )
+        self.layout.addWidget( self.btnAddCategoria)
+
 
     def abrirTelaCategoria(self):
         self.telaCategorias.show()
+
     def salvar(self):
         modelo = self.txtModelo.text() 
-        if modelo != "" :
+        if modelo != "" and self.cmbCategoria.currentIndex != 0:
             ano = self.txtAno.text()
             valor = None
             if ano != "":
@@ -44,5 +47,12 @@ class TelaCarro( TelaVeiculo ):
             vPortas = None
             if portas != "":
                 vPortas = int( portas ) 
-            carro = Carro(modelo, ano, vPortas)
+            cat = self.cmbCategoria.currentData()
+            carro = Carro(modelo, ano, vPortas, cat)
             QMessageBox.information(self, "Carro Salvo", str(carro) )
+    def carregarCategorias(self):
+        self.cmbCategoria.clear()
+        self.cmbCategoria.addItem( "Selecione...", None)
+        for cat in self.listaCategorias:
+            self.cmbCategoria.addItem( cat.nome , cat)
+    
--- Aula04 - Herança/TelaCategoria.py
+++ Aula04 - Herança/TelaCategoria.py
@@ -1,12 +1,14 @@
 import sys
-#from PyQt5.QtWidgets import QPushButton, QLineEdit
 from PyQt5.QtWidgets import *
 from Categoria import Categoria
+from TelaCarro import TelaCarro
 
 class TelaCategoria( QMainWindow ):
-    def __init__(self, titulo = "Tela de Categoria", categorias = [] ):
-        super().__init__()
+    def __init__(self, titulo = "Tela de Categoria", categorias = [], telaCar = None):
+        self.telaCarro = telaCar
         self.categorias = categorias
+        
+        super().__init__()
         self.setWindowTitle( titulo )
         self.setGeometry( 300, 200, 150, 150 )
         self.layout = QVBoxLayout()
@@ -15,7 +17,7 @@ class TelaCategoria( QMainWindow ):
         self.btnSalvar = QPushButton("Salvar", self)
         self.btnSalvar.clicked.connect( self.salvar )
         self.layout.addWidget( self.btnSalvar)
-
+        
         container = QWidget()
         container.setLayout( self.layout )
         self.setCentralWidget( container )
@@ -24,13 +26,16 @@ class TelaCategoria( QMainWindow ):
         nome = self.txtNome.text() 
         if nome != "" :
             cat = Categoria( nome )
-            self.categorias.append(cat)
+            self.categorias.append( cat )
             QMessageBox.information(self, "Categoria Salva", str(cat) )
+            if self.telaCarro is not None:
+                self.telaCarro.carregarCategorias()
+                self.txtNome.setText("")
             self.hide()
 
 
     def definirLayout(self):
-        self.lblNome = QLabel("Modelo: ")
+        self.lblNome = QLabel("Nome: ")
         self.txtNome = QLineEdit(self)
-        self.layout.addWidget( self.lblNome )
+        self.layout.addWidget( self.lblNome)
         self.layout.addWidget( self.txtNome )
