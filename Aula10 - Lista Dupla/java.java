bd9e86bbdd34ef2588f9511e58b32aa0ce8970ea
NICOLAS DIOVANI OLIVEIRA DIAS <02363124057@senacrs.edu.br>
2025-06-04 11:24:09 -0300

    atividade: fazer uma lista duplamente encadiada decrescente

--- Aula10 - Lista Dupla/java.java
+++
@@ -1,6 +0,0 @@
-public class java {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!"); // Exemplo de saída
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    }
-}
---
+++ Aula10 - Lista Dupla/main.py
@@ -0,0 +1,9 @@
+from ListaDuplamente import ListaDuplamente
+lista = ListaDuplamente()
+lista.add("nicolas")
+lista.add("ana")
+lista.add("joao")
+lista.add("maria")
+lista.add("pedro")
+lista.add("luan")
+lista.imprimir()
