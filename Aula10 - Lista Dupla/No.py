4608dddf4d94ef8b503270708f763d1f06733ac3
NICOLAS DIOVANI OLIVEIRA DIAS <02363124057@senacrs.edu.br>
2025-06-04 10:14:57 -0300

    intervalo

--- Aula10 - Lista Dupla/No.py
+++ Aula10 - Lista Dupla/No.py
@@ -3,3 +3,29 @@ class No:
         self.ant = None
         self.dado = valor
         self.prox = None
+
+    def add(self, valor):
+        nodo = No( valor )
+        if self.inicio is None:
+            self.inicio = nodo
+            self.fim = nodo
+        elif nodo.dado < self.inicio.dado:
+            nodo.prox = self.inicio
+            self.inicio.ant = nodo
+            self.inicio = nodo
+        elif nodo.dado > self.fim.dado:
+            nodo.ant = self.fim
+            self.fim.prox = nodo
+            self.fim = nodo
+        else:
+            antTestado = self.inicio
+            aux = self.inicio.prox
+            while aux:
+                if nodo.dado < aux.dado:
+                    nodo.prox = aux
+                    nodo.ant = antTestado
+                    antTestado.prox = nodo
+                    aux.ant = nodo
+                else: 
+                    antTestado = aux
+                    aux = aux.prox
